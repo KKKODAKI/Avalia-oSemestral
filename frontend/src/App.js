7b69cb4b57e246e7d4e56dab53a0b6354a4ea380
KKKODAKI <l.tieppo10@gmail.com>
2024-12-02 20:12:10 -0300

    aba produtos com login melhorada

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,9 +7,12 @@ import UpdateProductByID from './UpdateProductByID';
 import UserLogin from './UserLogin';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
+
 function App() {
   const [currentPage, setCurrentPage] = useState('landing');
   const [showMenu, setShowMenu] = useState(false);
+  const [showLogin, setShowLogin] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
@@ -20,17 +23,12 @@ function App() {
   }, []);
 
   const handleNavClick = (page) => {
-    if (page === 'createProducts' && !isLoggedIn) {
-      setCurrentPage('login');
-    } else {
-      setCurrentPage(page);
-    }
+    setShowMenu(false);
+    setShowLogin(false);
+    setCurrentPage(page);
   }
 
   const handleMenuClick = (option) => {
-    if (!isLoggedIn && (option === 'createProducts' || option === 'showAllProducts'  || option === 'deleteProductById' || option === 'updateProductById')) {
-      setCurrentPage('login');
-    } else {
       switch (option) {
         case 'createProducts':
           setCurrentPage('createProducts');
@@ -47,12 +45,13 @@ function App() {
         default:
           break;
       }
-    }
     setShowMenu(false);
   }
 
   const handleLogout = () => {
     localStorage.removeItem('token');
+    setShowMenu(false);
+    setShowLogin(false);
     setIsLoggedIn(false);
     setCurrentPage('landing');
   }
@@ -60,7 +59,7 @@ function App() {
   return (
     <div className="App">
       <nav className="navbar navbar-expand-lg navbar-light bg-secondary bg-gradient">
-        <a className="navbar-brand ms-2 text-dark" href="#" onClick={() => handleNavClick('landing')}>Landing Page</a>
+        <a className="navbar-brand ms-2 text-dark" href="landing">Landing Page</a>
         <div className="collapse navbar-collapse">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
@@ -73,7 +72,8 @@ function App() {
               <button className="nav-link btn me-2" onClick={handleLogout}>Sair</button>
             </li>
             <li className='nav-item'>
-              <button className='nav-link btn' onClick={() => setShowMenu(!showMenu)}>Produtos</button>
+              {/*<button className='nav-link btn' onClick={() => setShowMenu(isLoggedIn ? !showMenu : showMenu) && setShowLogin(!isLoggedIn ? !showLogin : showLogin)}>Produtos</button>*/}
+              <button className='nav-link btn' onClick={() => isLoggedIn ? setShowMenu(!showMenu) : setShowLogin(!showLogin)}>Produtos</button>
             </li>
           </ul>
         </div>
@@ -81,11 +81,23 @@ function App() {
 
       {/* Menu Flutuante */}
       {showMenu && (
-        <div className="dropdown-menu show" style={{ position: 'absolute', right: '10px', top: '50px' }}>
+        <div className="dropdown-menu show" style={{ position: 'absolute', right: '5px', top: '60px' }}>
           <button className="dropdown-item" onClick={() => handleMenuClick('createProducts')}>Novo produto</button>
           <button className="dropdown-item" onClick={() => handleMenuClick('showAllProducts')}>Mostrar todos os produtos</button>
           <button className="dropdown-item" onClick={() => handleMenuClick('deleteProductById')}>Deletar produto</button>
           <button className="dropdown-item" onClick={() => handleMenuClick('updateProductById')}>Atualizar produto</button>
+        </div>
+      )}
+
+      {/* Menu Flutuante */}
+      {showLogin && (
+        <div className="dropdown-menu show bg-light" style={{ position: 'absolute', right: '5px', top: '60px' }}>
+          <p className=" mx-3 my-1 text-danger" href="#">
+            <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" class="bi bi-exclamation-triangle-fill" viewBox="0 0 16 16">
+              <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5m.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
+            </svg>
+            Login não efetuado
+          </p>
         </div>
       )}
 
